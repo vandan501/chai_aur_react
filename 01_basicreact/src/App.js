6ffab91b5f3f3b7d387119865def25e573463188
Raval Vandan <vandanraval2002@gmail.com>
2024-01-11 11:04:39 +0530

    basic react added

---
+++ 01_basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+function App() {
+  return ( <>
+   <h1>Hello world </h1> Vandan raval here
+ </>
+  );
+}
+
+export default App;
---
+++ 01_vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <h1>
+    Hello world |vandan raval
+    </h1>
+  )
+}
+
+export default App
